a27e06eefbe3cb6b6b6b3a977a22e9e565e3d9fb
Samuel Brennan <samjbren@gmail.com>
2017-02-19 19:53:52 +0000

    Refactor fucntional tests

--- tests/functional_tests.py
+++ tests/functional_tests.py
@@ -1,7 +1,5 @@
-"""Something
-"""
+# pylint: disable=C0111
 import unittest
-
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 
@@ -10,33 +8,53 @@ class NewVisitorTest(unittest.TestCase):
 
     def setUp(self):
         self.browser = webdriver.Chrome()
-        self.browser.implicitly_wait(3)
+        # self.browser.implicitly_wait(3)
 
     def tearDown(self):
         self.browser.quit()
 
+    # pylint: disable=C0103
     def test_can_start_a_list_and_retrieve_it_later(self):
-        # Laura has found a cool new to-do list web-app so she visits the
-        # homepage
+
+        self.a_new_user_visits_the_homepage()
+        self.user_notices_the_title_and_header()
+
+        inputbox = self.she_can_enter_a_new_item_in_the_list_straight_away()
+        # self.she_enters_eat_pizza_into_the_textbox(inputbox)
+        # self.pressing_enter_updates_the_todo_list_with_first_item(inputbox)
+
+        # She can still add new items via a text box so she enters "Clean up
+        # after pizza"
+        # self.fail("Finish the test!")
+
+        # The page is updated to show both entries on the screen with the new one being
+        # second.
+
+        # She wants to save the list for future and notices that the site has created
+        # a unique link with a brief explanation.
+
+        # She tries out the URL just to be sure it works and it does
+
+    def a_new_user_visits_the_homepage(self):
         self.browser.get('http://localhost:8000')
 
-        # She notices the title and the header
-        self.assertIn('ToDo', self.browser.title)
+    def user_notices_the_title_and_header(self):
+        self.assertIn('To-Do', self.browser.title)
         header_text = self.browser.find_element_by_tag_name('h1').text
-        self.assertIn('ToDo', header_text)
+        self.assertIn('To-Do', header_text)
 
-        # She can enter a new item to the list straight away
+    def she_can_enter_a_new_item_in_the_list_straight_away(self):
         inputbox = self.browser.find_element_by_id('id_new_item')
         self.assertEqual(
             inputbox.get_attribute('placeholder'),
             'Enter a to-do item'
         )
+        return inputbox
 
-        # She enters "Eat pizza" into the textbox
+    def she_enters_eat_pizza_into_the_textbox(self, inputbox):
         inputbox.send_keys('Eat pizza')
 
-        # When she hits enter, the page updates with the first item in the TODO list.
-        # It lists "1: Eat pizza" as the first item.
+    def pressing_enter_updates_the_todo_list_with_first_item(self, inputbox):
         inputbox.send_keys(Keys.ENTER)
 
         table = self.browser.find_element_by_id('id_list_table')
@@ -45,18 +63,6 @@ class NewVisitorTest(unittest.TestCase):
             any(row.text == '1: Eat pizza' for row in rows)
         )
 
-        # She can still add new items via a text box so she enters "Clean up after pizza"
-        self.fail("Finish the test!")
-
-        # The page is updated to show both entries on the screen with the new one being
-        # second.
-
-
-        # She wants to save the list for future and notices that the site has created
-        # a unique link with a brief explanation.
-
-
-        # She tries out the URL just to be sure it works and it does
 
 if __name__ == '__main__':
     unittest.main(warnings='ignore')
